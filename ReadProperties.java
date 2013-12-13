6763a49d561553ada9923a628cc86d8c561142b9
Javier <javier.molina@buongiorno.com>
2013-12-13 10:16:33 +0100

    Nueva clase de lectura de properties

---
+++ ReadProperties.java
@@ -0,0 +1,29 @@
+package com.buongiorno.utils;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class ReadProperites {
+	
+	private String mensaje;
+
+
+	private void readConfiguration()throws IOException {
+
+		Properties config = new Properties();
+
+		config.load(this.getClass().getResourceAsStream("/smal_project.properties"));
+		mensaje = config.getProperty("website");
+		
+	}
+		
+	public String getmensaje() {
+		return mensaje;
+	}
+
+	public void setmensaje(String mensaje) {
+		this.mensaje = mensaje;
+	}
+
+
+}
